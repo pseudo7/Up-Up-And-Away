a6a103d97a3101c1c9f9aea7f949230c9e52ecac
Pseudo <enikhil7@gmail.com>
2019-03-11 23:21:52 +0530

    Improved Gameplay

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class PlatformManager : MonoBehaviour
 {
-    public static Level currentLevel = Level.Level1;
+    public static Level currentLevel = Level.Level3;
 
     public GameObject sectorPrefab;
     public GameObject finishPrefab;
     public Transform wallTransform;
+    public Transform pillarTransform;
     public Vector3 origin;
+    public Material wallMat;
 
     static Transform lastSpawnedSegment;
     static int currentHeightIndex;
@@ -18,7 +20,7 @@ public class PlatformManager : MonoBehaviour
     {
         currentHeightIndex = 0;
         lastSpawnedSegment = null;
-        UIManager.Instance.UpdateLevelText(currentLevel.ToString());
+        UIManager.Instance.UpdateLevelText(currentLevel);
         CreatePlatforms(LevelManager.Instance.LoadLevel(currentLevel));
     }
 
@@ -34,12 +36,27 @@ public class PlatformManager : MonoBehaviour
         currentHeightIndex++;
     }
 
+    private void LateUpdate()
+    {
+        wallMat.mainTextureOffset = new Vector2(0, Time.time);
+    }
+
     public void CreatePlatforms(LevelInfo level)
     {
         foreach (PlatformInfo info in level.platforms) CreatePlatform(info);
         Instantiate(finishPrefab, origin + Vector3.up * level.platformCount * Constants.PLATFORM_HEIGHT, Quaternion.identity);
         wallTransform.localScale = new Vector3(1, level.platformCount * Constants.PLATFORM_HEIGHT, 1);
+        pillarTransform.localScale = new Vector3(1, level.platformCount * Constants.PLATFORM_HEIGHT / 2, 1);
+        wallMat.mainTextureScale = new Vector2(5, level.platformCount * Constants.PLATFORM_HEIGHT / 2);
     }
+
+#if UNITY_EDITOR
+    [UnityEditor.MenuItem("Pseudo/Capture")]
+    static void Capture()
+    {
+        ScreenCapture.CaptureScreenshot(string.Format("{0}.png", System.DateTime.Now.Ticks.ToString()));
+    }
+#endif
 }
 
 public struct PlatformInfo
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,15 +8,26 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance;
 
     [SerializeField] Text levelText;
+    [SerializeField] Text timeText;
 
-    private void Awake()
+    void Awake()
     {
         if (!Instance)
             Instance = this;
     }
 
-    public void UpdateLevelText(string levelName)
+    void Update()
     {
-        levelText.text = levelName;
+        UpdateLevelTime();
+    }
+
+    public void UpdateLevelText(Level levelName)
+    {
+        levelText.text = string.Format("Level {0}", (int)(levelName + 1));
+    }
+
+    void UpdateLevelTime()
+    {
+        timeText.text = Time.timeSinceLevelLoad.ToString("00#.00");
     }
 }
